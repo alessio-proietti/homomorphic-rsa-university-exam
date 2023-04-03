167a1f704917617ced0c92ab3f643bf67319956a
Alessio Proietti <alessio.proietti@protonmail.com>
2023-04-03 17:01:33 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// innanzitutto occorre implementare fare quadrato modulare 
+
+using System;
+
+namespace HomomorphicRSA
+{
+    class RSACrypto
+    {
+        
+        static void Main(string[] args)
+        {
+            int message = 50;
+            int modulus = 97;
+
+            Console.WriteLine(message*message%modulus);
+        }
+    }
+}
